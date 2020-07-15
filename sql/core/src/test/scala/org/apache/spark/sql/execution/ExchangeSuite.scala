20770027e6f55e66eff5ae44cec9299dbdf464c6
Andy Grove <andygrove@nvidia.com>
2020-07-15 13:40:34 -0600

    revert another change

--- sql/core/src/test/scala/org/apache/spark/sql/execution/ExchangeSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/ExchangeSuite.scala
@@ -39,7 +39,7 @@ case class ColumnarExchange(child: SparkPlan) extends Exchange {
 
   override protected def doExecute(): RDD[InternalRow] = throw new RanRowBased
 
-  override protected[sql] def doExecuteColumnar(): RDD[ColumnarBatch] = throw new RanColumnar
+  override protected def doExecuteColumnar(): RDD[ColumnarBatch] = throw new RanColumnar
 }
 
 class ExchangeSuite extends SparkPlanTest with SharedSparkSession {
